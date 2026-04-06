8bcf471a5aafbe941bdc7bec5b166b953b5371f8
Taj-786 <tajuddinshaik786r@gmail.com>
2026-04-06 19:09:38 +0530

    fixed the lint issues for server

--- server/src/interfaces/IAuthTokenProvider.ts
+++ server/src/interfaces/IAuthTokenProvider.ts
@@ -1,4 +1,4 @@
 import type { ITokenService } from "./ITokenService";
 
 /** Full token stack for a single deployment (JWT, OAuth-JWT, etc.). */
-export interface IAuthTokenProvider extends ITokenService {}
+export type IAuthTokenProvider = ITokenService;
